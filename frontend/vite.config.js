d30949880d877342f36ce98c447e9282f64cfce7
s11ngh <ujjwalwrk@gmail.com>
2024-09-15 08:23:11 -0400

    Encryption with IBM

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  jsxRuntime: "classic", // Add this line
-
-  plugins: [react()],
-});
